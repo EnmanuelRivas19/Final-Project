60998bb7d8157eed18d3f72045248c33ef1e3e67
EnmanuelRivas19 <enmanu.rivas@newhorizons.edu.do>
2021-12-13 22:05:56 -0400

    por favor yo no se a ete punto que toy haciendo ??

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,27 @@ import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavHeaderComponent } from './nav-header/nav-header.component';
+import { DatePickerComponent } from './date-picker/date-picker.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { HalfDivComponent } from './half-div/half-div.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CarouselComponent,
     FooterComponent,
-    NavHeaderComponent
+    NavHeaderComponent,
+    DatePickerComponent,
+    HalfDivComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
